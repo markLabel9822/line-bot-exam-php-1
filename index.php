a85f5d58bd36048cd997fc9e9f4ad9f409ea48a9
markLabel9822 <45809781+markLabel9822@users.noreply.github.com>
2018-12-14 17:13:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -124,6 +124,14 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 	<h2 class="sub-head">Register</h2>
 		<div class="sub-main">	
 			<form action="#" method="post">
+
+
+<?php 
+
+ echo "Line UserID: ".$lineUserData['sub']."<br>";
+?>
+
+
 				<input placeholder="First Name" name="Name" class="name" type="text" required="">
 					<span class="icon1"><i class="fa fa-user" aria-hidden="true"></i></span><br>
 				<input placeholder="Last Name" name="Name" class="name2" type="text" required="">
