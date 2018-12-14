fe0bc4567e19913d805ccb7a43b07e9df3ed0f23
markLabel9822 <45809781+markLabel9822@users.noreply.github.com>
2018-12-14 17:01:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+
+
+
 session_start();
 require_once("LineLoginLib.php");
  
@@ -37,11 +41,13 @@ if($LineLogin->verifyToken($accToken)){
  
  
 //////////////////////////
+
 // GET LINE USERID FROM USER PROFILE
 //$userID = $LineLogin->userProfile($accToken);
 //echo $userID;
  
 //////////////////////////
+
 // GET LINE USER PROFILE 
 /*$userInfo = $LineLogin->userProfile($accToken,true);
 if(!is_null($userInfo) && is_array($userInfo) && array_key_exists('userId',$userInfo)){
@@ -49,18 +55,22 @@ if(!is_null($userInfo) && is_array($userInfo) && array_key_exists('userId',$user
 }
  
 //exit;*/
+
  
 if(isset($_SESSION['ses_login_userData_val']) && $_SESSION['ses_login_userData_val']!=""){
     // GET USER DATA FROM ID TOKEN
     $lineUserData = json_decode($_SESSION['ses_login_userData_val'],true);
    // print_r($lineUserData); 
     //echo "<hr>";
+ 
+    //แสดงชื่่อ UserID
     echo "Line UserID: ".$lineUserData['sub']."<br>";
    
  
 }
  
  
+
 if(isset($_SESSION['ses_login_refreshToken_val']) && $_SESSION['ses_login_refreshToken_val']!=""){
    
 }
@@ -83,91 +93,10 @@ if(isset($_SESSION['ses_login_refreshToken_val']) && $_SESSION['ses_login_refres
 //$statusRevoke = $LineLogin->revokeToken($accToken, true);
 //print_r($statusRevoke);
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<head>
-<title>Flat Sign Up Form Responsive Widget Template| Home :: W3layouts</title>
-<!-- Meta tag Keywords -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Flat Sign Up Form Responsive Widget Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- Meta tag Keywords -->
-<!-- css files -->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
-<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
-<!-- //css files -->
-<!-- online-fonts -->
-<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'><link href='//fonts.googleapis.com/css?family=Raleway+Dots' rel='stylesheet' type='text/css'>
-</head>
-<body>
-<!--header-->
-	<div class="header-w3l">
-		<h1>Valet Parking</h1>
-
-	</div>
-<!--//header-->
-<!--main-->
-<div class="main-agileits">
-	<h2 class="sub-head">Register</h2>
-		<div class="sub-main">	
-			<form action="#" method="post">
-
-				
-		<?php
+<?php
 echo "<hr>";
 if($LineLogin->verifyToken($accToken)){
 ?>
-				<input placeholder="First Name" name="Name" class="name" type="text" required="">
-					<span class="icon1"><i class="fa fa-user" aria-hidden="true"></i></span><br>
-				<input placeholder="Last Name" name="Name" class="name2" type="text" required="">
-					<span class="icon2"><i class="fa fa-user" aria-hidden="true"></i></span><br>
-				<input placeholder="Phone Number" name="Number" class="number" type="text" required="">
-					<span class="icon3"><i class="fa fa-phone" aria-hidden="true"></i></span><br>
-				<input placeholder="Email" name="mail" class="mail" type="text" required="">
-					<span class="icon4"><i class="fa fa-envelope" aria-hidden="true"></i></span><br>
-				<input  placeholder="Password" name="Password" class="pass" type="password" required="">
-					<span class="icon5"><i class="fa fa-unlock" aria-hidden="true"></i></span><br>
-				<input  placeholder="Confirm Password" name="Password" class="pass" type="password" required="">
-					<span class="icon6"><i class="fa fa-unlock" aria-hidden="true"></i></span><br>
-				
-				<input type="submit" value="sign up">
-			</form>
-		</div>
-		<div class="clear"></div>
-</div>
-<!--//main-->
-
-<!--footer-->
-<div class="footer-w3">
-	<p>&copy; 2018 Valet Parking . All rights reserved | Design by MarkLabel</a></p>
-</div>
-<!--//footer-->
-
-</body>
-</html>
-
-
-
-
-
-
-
 <form method="post">
 <button type="submit" name="lineLogout">LINE Logout</button>
 </form>
