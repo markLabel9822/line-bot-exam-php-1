20e42b2d7d8a21334dca5d40c7290f209bfa6e6b
markLabel9822 <45809781+markLabel9822@users.noreply.github.com>
2018-12-14 17:57:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -74,15 +74,6 @@ if(isset($_SESSION['ses_login_userData_val']) && $_SESSION['ses_login_userData_v
  
 
  
-if(isset($_SESSION['ses_login_userData_val']) && $_SESSION['ses_login_userData_val']!=""){
-    // GET USER DATA FROM ID TOKEN
-    $lineUserData = json_decode($_SESSION['ses_login_userData_val'],true);
-    print_r($lineUserData); 
-   // echo "<hr>";
-    echo "Line UserID: ".$lineUserData['sub']."<br>";
-   
-}
- 
  
 
 
@@ -128,15 +119,11 @@ if(isset($_POST['lineLogout'])){
         $_SESSION['ses_login_refreshToken_val'],
         $_SESSION['ses_login_userData_val']
     );  
-    echo "<hr>";
+   
     if($LineLogin->revokeToken($accToken)){
-        echo "Logout Line Success<br>";   
+         
     }
-    echo '
-    <form method="post">
-    <button type="submit" name="lineLogin">LINE Login</button>
-    </form>   
-    ';
+  
     $LineLogin->redirect("login_uselib.php");
 }
 ?>
