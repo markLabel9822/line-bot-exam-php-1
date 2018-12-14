8c79048d85d5a443200f178ce447aff02f255616
markLabel9822 <45809781+markLabel9822@users.noreply.github.com>
2018-12-14 15:38:23 +0700

    Update login_uselib.php

--- login_uselib.php
+++ login_uselib.php
@@ -1,8 +1,4 @@
 <?php
-
-
-
-
 session_start();
 require_once("LineLoginLib.php");
  
@@ -30,12 +26,12 @@ if(!isset($_SESSION['ses_login_accToken_val'])){
 $accToken = $_SESSION['ses_login_accToken_val'];
 // Status Token Check
 if($LineLogin->verifyToken($accToken)){
- 
-    echo "Toke Status  <br>";  
+    echo $accToken."<br><hr>";
+    echo "Token Status OK <br>";  
 }
  
  
-
+echo "<pre>";
 // Status Token Check with Result 
 //$statusToken = $LineLogin->verifyToken($accToken, true);
 //print_r($statusToken);
@@ -48,7 +44,7 @@ echo "<hr>";
 //echo $userID;
  
 //////////////////////////
-
+echo "<hr>";
 // GET LINE USER PROFILE 
 $userInfo = $LineLogin->userProfile($accToken,true);
 if(!is_null($userInfo) && is_array($userInfo) && array_key_exists('userId',$userInfo)){
@@ -56,20 +52,20 @@ if(!is_null($userInfo) && is_array($userInfo) && array_key_exists('userId',$user
 }
  
 //exit;
-
+echo "<hr>";
  
 if(isset($_SESSION['ses_login_userData_val']) && $_SESSION['ses_login_userData_val']!=""){
     // GET USER DATA FROM ID TOKEN
     $lineUserData = json_decode($_SESSION['ses_login_userData_val'],true);
     print_r($lineUserData); 
-   
+    echo "<hr>";
     echo "Line UserID: ".$lineUserData['sub']."<br>";
-   
- 
+    echo "Line Display Name: ".$lineUserData['name']."<br>";
+    echo '<img style="width:100px;" src="'.$lineUserData['picture'].'" /><br>';
 }
  
  
-
+echo "<hr>";
 if(isset($_SESSION['ses_login_refreshToken_val']) && $_SESSION['ses_login_refreshToken_val']!=""){
     echo '
     <form method="post">
@@ -97,7 +93,7 @@ if(isset($_SESSION['ses_login_refreshToken_val']) && $_SESSION['ses_login_refres
 //print_r($statusRevoke);
 ?>
 <?php
-
+echo "<hr>";
 if($LineLogin->verifyToken($accToken)){
 ?>
 <form method="post">
@@ -107,7 +103,6 @@ if($LineLogin->verifyToken($accToken)){
 <form method="post">
 <button type="submit" name="lineLogin">LINE Login</button>
 </form>   
-
 <?php } ?>
 <?php
 if(isset($_POST['lineLogin'])){
